24af9da14b7b61d6552bd338c6b5b6b00eb8638f
misiek03 <mgrygiel@onet.pl>
2025-06-13 17:58:25 +0200

    add missing assets

---
+++ assets/maps/MainMap.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="MainMap" tilewidth="128" tileheight="128" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../assets/Tile/1.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../assets/Tile/2.png" width="128" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="../assets/Tile/3.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="../assets/Tile/4.png" width="128" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="../assets/Tile/5.png" width="128" height="128"/>
+ </tile>
+ <tile id="5">
+  <image source="../assets/Tile/6.png" width="128" height="128"/>
+ </tile>
+ <tile id="6">
+  <image source="../assets/Tile/7.png" width="128" height="128"/>
+ </tile>
+ <tile id="7">
+  <image source="../assets/Tile/8.png" width="128" height="128"/>
+ </tile>
+ <tile id="8">
+  <image source="../assets/Tile/9.png" width="128" height="128"/>
+ </tile>
+ <tile id="9">
+  <image source="../assets/Tile/10.png" width="128" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="../assets/Tile/11.png" width="128" height="128"/>
+ </tile>
+ <tile id="11">
+  <image source="../assets/Tile/12.png" width="128" height="128"/>
+ </tile>
+ <tile id="12">
+  <image source="../assets/Tile/13.png" width="128" height="128"/>
+ </tile>
+ <tile id="13">
+  <image source="../assets/Tile/14.png" width="128" height="93"/>
+ </tile>
+ <tile id="14">
+  <image source="../assets/Tile/15.png" width="128" height="93"/>
+ </tile>
+ <tile id="15">
+  <image source="../assets/Tile/16.png" width="128" height="93"/>
+ </tile>
+</tileset>
---
+++ assets/maps/TilesetDesert.tsx
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TilesetDesert" tilewidth="48" tileheight="48" tilecount="24" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../Tile/Desert/1.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../Tile/Desert/2.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../Tile/Desert/3.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../Tile/Desert/4.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../Tile/Desert/5.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../Tile/Desert/6.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../Tile/Desert/7.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../Tile/Desert/8.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../Tile/Desert/9.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../Tile/Desert/10.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../Tile/Desert/11.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="../Tile/Desert/12.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="../Tile/Desert/13.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="../Tile/Desert/14.png" width="32" height="23"/>
+ </tile>
+ <tile id="14">
+  <image source="../Tile/Desert/15.png" width="32" height="23"/>
+ </tile>
+ <tile id="15">
+  <image source="../Tile/Desert/16.png" width="32" height="23"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="Player"/>
+  </properties>
+  <image source="../Player/Woodcutter/Woodcutter.png" width="48" height="48"/>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="E_Deceased"/>
+  </properties>
+  <image source="../Enemy/Deceased/Deceased.png" width="48" height="48"/>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="E_Hyena"/>
+  </properties>
+  <image source="../Enemy/Hyena/Hyena.png" width="48" height="48"/>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="name" value="E_Mummy"/>
+  </properties>
+  <image source="../Enemy/Mummy/Mummy.png" width="48" height="48"/>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="name" value="E_Scorpio"/>
+  </properties>
+  <image source="../Enemy/Scorpio/Scorpio.png" width="48" height="48"/>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="name" value="E_Snake"/>
+  </properties>
+  <image source="../Enemy/Snake/Snake.png" width="48" height="48"/>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="name" value="E_Vulture"/>
+  </properties>
+  <image source="../Enemy/Vulture/Vulture.png" width="48" height="48"/>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="name" value="Coin"/>
+  </properties>
+  <image source="../../../../Downloads/single_coin.png" width="32" height="32"/>
+ </tile>
+</tileset>
